656d20185447532fe7237b47cf8840156205c694
Ismael González Sierra <ismael.gonzalez@babel.es>
2015-09-28 13:29:44 +0200

    Initial commit navigation library

---
+++ src/main/java/library/navigation/interfaces/Exceptions.java
@@ -0,0 +1,8 @@
+package library.navigation.interfaces;
+
+@SuppressWarnings("unused")
+public interface Exceptions {
+
+    String CONTAINER_EXCEPTION = "Container undefined.";
+    String SIZE_STACK_EXCEPTION = "Stack size exceeded.";
+}
